d0487342cfdd7c3e63f1f48da350bf4fe4f168e8
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-11-16 10:12:20 -0700

    add /public_html files per the Angular 2 Demo.

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>Welcome to the Angular 2 Template Demo!</h1>
+			<p class="lead">This is the HOME view! :D</p>
+		</div>
+	</div>
+</section>
